2f1304c7614a273660de2eea3a012df82b6c0a9d
flklein <flo-klein@hotmail.fr>
2019-06-17 16:39:14 +0200

    fresh start

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,18 @@
-import React, { Component } from 'react'
-import { Provider } from 'react-redux'
-import logo from './logo.svg'
-import './App.css'
-import store from './store'
-import Customers from './components/Customer/customers'
+import React, {Component} from 'react';
+import {Provider} from 'react-redux';
+import store from './store';
+
+import Home from './components/Home/Home';
 
 class App extends Component {
 
-  render () {
+  render() {
     return (
-      <Provider store={ store }>
-        <div className="App">
-          <header className="App-header">
-            <img src={ logo } className="App-logo" alt="logo"/>
-            <h1 className="App-title">React/Redux Express Starter</h1>
-          </header>
-          <Customers/>
-        </div>
+      <Provider store={store}>
+        <Home/>
       </Provider>
     )
   }
 }
 
-export default App
+export default App;
