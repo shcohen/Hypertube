cd473e8cbd6981835f650444e0dc666821a7796b
yadouble <yadouble@student.42.fr>
2019-07-19 16:42:24 +0200

    Streaming begin

--- Routes/torrentManagement.js
+++ Routes/torrentManagement.js
@@ -1,6 +1,11 @@
 const torrentSearch = require('torrent-search-api');
+const crypto = require('crypto');
+const mime = require('mime');
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffmpeg = require('fluent-ffmpeg');
+ffmpeg.setFfmpegPath(ffmpegPath);
+const pump = require('pump');
 const torrentStream = require('torrent-stream');
-const md5 = require('reverse-md5');
 const {removeTitleAndQualityDoublons, regroupTorrent} = require('../utils/moviesUtils');
 
 module.exports = {
@@ -23,32 +28,98 @@ module.exports = {
             return res.status(200).send('Wrong data sent');
         }
     },
+    streamVideoWithConversion: (res, file, fileSize, start, end) => {
+        let stream = file.createReadStream({start, end});
+        let video = ffmpeg(stream)
+            .videoCodec('libvpx')
+            .audioCodec('libvorbis')
+            .format('mp4')
+            .on('progress', function (progress) {
+                console.log('Processing: ' + progress.percent + '% done');
+            })
+            .on('error', function (err) {
+                console.log('Cannot process video: ' + err.message);
+            })
+            .on('end', function () {
+                console.log('Processing finished successfully');
+            });
+        pump(video, res);
+    },
+    streamVideoWithoutConversion: (res, file, fileSize, start, end, chunksize) => {
+        console.log('Streaming initialized...');
+        const head = {
+            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunksize,
+            'Content-Type': 'video/mp4',
+        };
+        res.writeHead(206, head);
+        console.log('Header writted !');
+        let stream = file.createReadStream(file, {start, end});
+        console.log('Stream created !');
+        stream.pipe(res);
+        console.log('Stream pumped !');
+    },
+    torrentDownloader: (res, range, directoryName, decoded, options) => {
+        let engine = torrentStream(decoded, options);
+        let parts = range.replace(/bytes=/, "").split("-");
+        let start = parseInt(parts[0], 10);
+        let data = '';
+        let fileSize;
+        engine.on('ready', () => {
+            engine.files.forEach(file => {
+                if (mime.getType(file.path) === 'video/mp4' || mime.getType(file.path) === 'video/ogg' || mime.getType(file.path) === 'video/webm') {
+                    console.log('Valid mimetype !');
+                    fileSize = file.length;
+                    let end = parts[1] ? parseInt(parts[1]) : fileSize - 1;
+                    const chunksize = (end - start) + 1;
+                    module.exports.streamVideoWithoutConversion(res, file, fileSize, start, end, chunksize);
+                } else if ((new RegExp(/video/)).test(mime.getType(file.path))) {
+                    fileSize = file.length;
+                    let end = parts[1] ? parseInt(parts[1]) : fileSize - 1;
+                    const chunksize = (end - start) + 1;
+                    module.exports.streamVideoWithConversion(res, file, fileSize, start, end, chunksize);
+                } else {
+                    file.deselect();
+                }
+                // let stream = file.createReadStream();
+                // stream.on('data', chunk => {
+                //     data += chunk;
+                // });
+                // stream.on('end', () => {
+                //     console.log(data);
+                // })
+            });
+            engine.on('download', () => {
+                console.log(engine.swarm.downloaded, fileSize);
+            })
+        });
+    },
     torrentManager: async (req, res) => {
         let {magnet} = req.params;
         let {range} = req.headers;
 
         if (magnet !== undefined && magnet.length || range === undefined) {
             let decoded = new Buffer(magnet, 'base64').toString();
-            let parts = range.replace(/bytes=/,"").split("-");
-            let start = parts[0];
-            let end = parts[1] ? parts[1] : undefined;
-            // let engine = torrentStream(magnet);
-            // engine.on('ready', () => {
-            //     engine.files.forEach(function(file) {
-            //         console.log('filename:', file.name);
-                    // let stream = file.createReadStream();
-                    // let data = '';
-                    // stream.on('data', chunk => {
-                    //     data += chunk
-                    // });
-                    // stream.on('end', () => {
-                    //     console.log(data)
-                    // })
-                // });
-            // });
-            // engine.on('download', piece => {
-            //   console.log('Piece: ', piece);
-            // });
+            if ((new RegExp(/magnet:\?xt=urn:.+/)).test(decoded)) {
+                let directoryName = crypto.createHash('md5').update(decoded).digest('hex');
+                let options = {
+                    connections: 100,
+                    uploads: 10,
+                    verify: true,
+                    dht: true,
+                    tracker: true,
+                    trackers: [
+                        'udp://tracker.openbittorrent.com:80',
+                        'udp://tracker.ccc.de:80',
+                        'udp://public.popcorn-tracker.org:6969/announce'
+                    ],
+                    path: `/tmp/torrentStream/${directoryName}`
+                };
+                module.exports.torrentDownloader(res, range, directoryName, decoded, options);
+            } else {
+                return res.status(200).send('Wrong magnet link !')
+            }
         } else {
             return res.status(200).send('Wrong data sent');
         }
--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connect('mongodb://localhost/db')
     console.log(e);
 });
 
-new CronJob('* * * * *', () => {
+new CronJob('* 1 * * *', () => {
     if (updateTrends() === -1) {
         console.log('Failed to update trends');
     } else {
@@ -40,7 +40,7 @@ app.use('/api', apiRouter);
 
 app.use('/', (req, res) => {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.end('<video src="http://localhost:3000/api/torrent/download_torrent/bWFnbmV0Oj94dD11cm46YnRpaDo3Q0MyNDlFQzBCNTA5NkZBN0EwQjE1QkI3M0I4MjkyRDdCMTZCNTNDJmRuPUJhdG1hbitOaW5qYSslMjgyMDE4JTI5KyU1QkJsdVJheSU1RCslNUIxMDgwcCU1RCslNUJZVFMlNUQrJTVCWUlGWSU1RCZ0cj11ZHAlM0ElMkYlMkZ0cmFja2VyLmNvcHBlcnN1cmZlci50ayUzQTY5NjklMkZhbm5vdW5jZSZ0cj11ZHAlM0ElMkYlMkY5LnJhcmJnLmNvbSUzQTI3MTAlMkZhbm5vdW5jZSZ0cj11ZHAlM0ElMkYlMkZwNHAuYXJlbmFiZy5jb20lM0ExMzM3JnRyPXVkcCUzQSUyRiUyRnRyYWNrZXIubGVlY2hlcnMtcGFyYWRpc2Uub3JnJTNBNjk2OSZ0cj11ZHAlM0ElMkYlMkZ0cmFja2VyLmludGVybmV0d2FycmlvcnMubmV0JTNBMTMzNyZ0cj11ZHAlM0ElMkYlMkZ0cmFja2VyLm9wZW50cmFja3Iub3JnJTNBMTMzNyUyRmFubm91bmNlJnRyPXVkcCUzQSUyRiUyRnRyYWNrZXIuemVyMGRheS50byUzQTEzMzclMkZhbm5vdW5jZSZ0cj11ZHAlM0ElMkYlMkZ0cmFja2VyLmxlZWNoZXJzLXBhcmFkaXNlLm9yZyUzQTY5NjklMkZhbm5vdW5jZSZ0cj11ZHAlM0ElMkYlMkZjb3BwZXJzdXJmZXIudGslM0E2OTY5JTJGYW5ub3VuY2U=" controls></video>');
+    res.end('<video src="http://localhost:3000/api/torrent/download_torrent/bWFnbmV0Oj94dD11cm46YnRpaDo3NjQzRDA2MjVERUQwQTVGQzk2N0IzN0E5RDZBRjY5OTAyMzZDMTgwJmRuPUF2ZW5nZXJzJTNBK0luZmluaXR5K1dhcislMjgyMDE4JTI5KyU1QldFQlJpcCU1RCslNUIxMDgwcCU1RCslNUJZVFMlNUQrJTVCWUlGWSU1RCZ0cj11ZHAlM0ElMkYlMkZ0cmFja2VyLmNvcHBlcnN1cmZlci50ayUzQTY5NjklMkZhbm5vdW5jZSZ0cj11ZHAlM0ElMkYlMkY5LnJhcmJnLmNvbSUzQTI3MTAlMkZhbm5vdW5jZSZ0cj11ZHAlM0ElMkYlMkZwNHAuYXJlbmFiZy5jb20lM0ExMzM3JnRyPXVkcCUzQSUyRiUyRnRyYWNrZXIubGVlY2hlcnMtcGFyYWRpc2Uub3JnJTNBNjk2OSZ0cj11ZHAlM0ElMkYlMkZ0cmFja2VyLmludGVybmV0d2FycmlvcnMubmV0JTNBMTMzNyZ0cj11ZHAlM0ElMkYlMkZ0cmFja2VyLm9wZW50cmFja3Iub3JnJTNBMTMzNyUyRmFubm91bmNlJnRyPXVkcCUzQSUyRiUyRnRyYWNrZXIuemVyMGRheS50byUzQTEzMzclMkZhbm5vdW5jZSZ0cj11ZHAlM0ElMkYlMkZ0cmFja2VyLmxlZWNoZXJzLXBhcmFkaXNlLm9yZyUzQTY5NjklMkZhbm5vdW5jZSZ0cj11ZHAlM0ElMkYlMkZjb3BwZXJzdXJmZXIudGslM0E2OTY5JTJGYW5ub3VuY2U=" controls></video>');
 });
 
 const API_PORT = process.env.API_PORT || 3000;
