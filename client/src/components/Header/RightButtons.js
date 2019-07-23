d9518d13e1eaa8ee93a3f32268b51fc2c553332a
Florent Klein <flklein@student.42.fr>
2019-07-23 18:24:11 +0200

    mobile version

--- client/src/components/Header/RightButtons.js
+++ client/src/components/Header/RightButtons.js
@@ -8,6 +8,22 @@ class RightButtons extends Component {
     opened2: false,
   };
 
+  componentDidMount() {
+    const tab = document.cookie.split(';');
+    let obj = {};
+    let tab2 = [];
+    tab.forEach(elem => {
+      tab2 = elem.split('=');
+      obj = {...obj, [tab2[0].trim()]:tab2[1].trim()}
+    });
+    document.documentElement.style.setProperty('--color-headings', obj.color);
+    document.documentElement.style.setProperty('--color-headings-transparent', obj.transparent);
+    document.documentElement.setAttribute('data-theme', obj.theme);
+    this.setState({
+      theme: obj.theme
+    });
+  }
+
   openDropdown = (n) => {
     if (n === 0) {
       this.setState({
@@ -23,11 +39,30 @@ class RightButtons extends Component {
 
   toggleMode = (e) => {
     document.documentElement.classList.add('transition');
+    const d = new Date();
+    d.setTime(d.getTime() + (7 * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + d.toUTCString();
     if (e.target.checked) {
       document.documentElement.setAttribute('data-theme', 'dark');
+      document.cookie = "theme=dark;" + expires + ";path=/";
+      this.setState({
+        theme: 'dark'
+      });
     } else {
       document.documentElement.setAttribute('data-theme', 'light');
+      document.cookie = "theme=light;" + expires + ";path=/";
+      this.setState({
+        theme: 'light'
+      });
     }
+    setTimeout(() => {
+      document.documentElement.classList.remove('transition');
+    }, 500);
+  };
+
+  toggleNeon = (e) => {
+    document.documentElement.classList.add('transition');
+    document.querySelector('a.logo').classList.toggle('neon');
     setTimeout(() => {
       document.documentElement.classList.remove('transition');
     }, 500);
@@ -36,10 +71,16 @@ class RightButtons extends Component {
   changeColor = (e) => {
     // const color = getComputedStyle(document.documentElement).getPropertyValue('--color-headings');
     document.documentElement.style.setProperty('--color-headings', e.target.value);
+    document.documentElement.style.setProperty('--color-headings-transparent', e.target.value + '80');
+    const d = new Date();
+    d.setTime(d.getTime() + (7 * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + d.toUTCString();
+    document.cookie = "color=" + e.target.value + ";" + expires + ";path=/";
+    document.cookie = "transparent=" + e.target.value + "80;" + expires + ";path=/";
   };
 
   render() {
-    const {loggedIn, opened1, opened2} = this.state;
+    const {loggedIn, opened1, opened2, theme} = this.state;
 
     if (loggedIn) {
       return (<React.Fragment>
@@ -56,7 +97,11 @@ class RightButtons extends Component {
             </div>
             <div className="dropdown__section dark-mode">
               Mode sombre
-              <input className="custom" type="checkbox" name="theme" onChange={this.toggleMode}/>
+              <input className="custom" type="checkbox" name="theme" onChange={this.toggleMode} checked={theme === 'dark'}/>
+            </div>
+            <div className="dropdown__section dark-mode">
+              Effet néon
+              <input className="custom" type="checkbox" name="neon" onChange={this.toggleNeon}/>
             </div>
             <div className="dropdown__section">
               Couleur secondaire
@@ -74,8 +119,8 @@ class RightButtons extends Component {
           </div>
         </div>
         <div className={classnames('account', {
-            'opened': opened2
-          })}>
+          'opened': opened2
+        })}>
           <button onClick={() => this.openDropdown(2)}/>
           <div className="dropdown">
             <div className="dropdown__title">
@@ -89,8 +134,39 @@ class RightButtons extends Component {
       </React.Fragment>);
     } else {
       return (<React.Fragment>
-        <div>
-          <button className="account"/>
+        <div className={classnames('dropdown__overlay', {
+          'shown': opened1 || opened2
+        })} onClick={() => this.openDropdown(0)}/>
+        <div className={classnames('settings', {
+          'opened': opened1
+        })}>
+          <button onClick={() => this.openDropdown(1)}/>
+          <div className="dropdown">
+            <div className="dropdown__title">
+              <i className="fas fa-palette"/> Thème
+            </div>
+            <div className="dropdown__section dark-mode">
+              Mode sombre
+              <input className="custom" type="checkbox" name="theme" onChange={this.toggleMode} checked={theme === 'dark'}/>
+            </div>
+            <div className="dropdown__section dark-mode">
+              Effet néon
+              <input className="custom" type="checkbox" name="neon" onChange={this.toggleNeon}/>
+            </div>
+            <div className="dropdown__section">
+              Couleur secondaire
+              <form className="color-picker">
+                <input type="radio" name="color" className="red" value="#ff2200" onChange={this.changeColor}/>
+                <input type="radio" name="color" className="orange" value="#ff7540" onChange={this.changeColor}/>
+                <input type="radio" name="color" className="yellow" value="#ffd500" onChange={this.changeColor}/>
+                <input type="radio" name="color" className="green" value="#73d15c" onChange={this.changeColor}/>
+                <input type="radio" name="color" className="blue" value="#0077ff" onChange={this.changeColor}/>
+                <input type="radio" name="color" className="indigo" value="#3F51B5" onChange={this.changeColor}/>
+                <input type="radio" name="color" className="purple" value="#c800ff" onChange={this.changeColor}/>
+                <input type="radio" name="color" className="pink" value="#ff66d4" onChange={this.changeColor}/>
+              </form>
+            </div>
+          </div>
         </div>
       </React.Fragment>);
     }
