5e88cf3a898c6ed77590129a664bc342c2444cd7
flklein <flo-klein@hotmail.fr>
2019-08-09 18:18:36 +0200

    search

--- client/src/components/Movies/Movies.js
+++ client/src/components/Movies/Movies.js
@@ -5,9 +5,42 @@ import './movies.css';
 class Movies extends Component {
   render() {
     return (
-        <div className="movies">
-          <input className="search-bar" type="text"/>
+      <div id="movies">
+        <div className="sidebar">
+          <div className="sidebar__icon"/>
+          <div className="sidebar__container">
+            <div className="sidebar__scrollable">
+              <div className="sidebar__title">
+                Filtres et tri
+              </div>
+              <hr/>
+              <form>
+                <label>Genre</label><br/>
+                <input/><br/>
+                <label>Genre</label><br/>
+                <input/><br/>
+                <label>Genre</label><br/>
+                <input/><br/>
+                <label>Genre</label><br/>
+                <input/><br/>
+                <label>Genre</label><br/>
+                <input/><br/>
+                <label>Genre</label><br/>
+                <input/><br/>
+                <label>Genre</label><br/>
+                <input/><br/>
+              </form>
+            </div>
+          </div>
         </div>
+        <div className="movies">
+          <input name="title"
+                 className="search-bar"
+                 type="text"
+                 minLength="1"
+                 maxLength="32"/>
+        </div>
+      </div>
     );
   }
 }
