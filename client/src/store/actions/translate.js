99ec120d055dfdddab96ffa302670d3b571628c1
Florent Klein <flklein@student.42.fr>
2019-08-29 13:11:43 +0200

    translate sped up

--- client/src/store/actions/translate.js
+++ client/src/store/actions/translate.js
@@ -16,18 +16,19 @@ export const getTranslation = () => async dispatch => {
   });
   const tab = Object.entries(english);
   let res = {};
-  for (const [page, textObj] of tab) {
+  await Promise.all(tab.map(async ([page, textObj]) => {
     let textArr = Object.entries(textObj);
     let tmp = {};
-    for (const [textName, textContent] of textArr) {
+    await Promise.all(textArr.map(async ([textName, textContent]) => {
       let response = await axios.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190827T162414Z.8f955b0b6efe7dc0.af0b47eb7ccf2bdb8e1b3f93191aa497966ad560'
         + `&text=${textContent}&lang=en-fr`);
       console.log(response.data);
       let textTranslated = response.data.text[0];
       tmp = {...tmp, [textName]: textTranslated};
-    }
+    }));
     res = {...res, [page]: tmp}
-  }
+  }));
+
   dispatch({
     type: GET_TRANSLATION,
     payload: res
