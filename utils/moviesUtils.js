e982ac483d27b3d44e55fe5bfb6b3650c2ae2cee
yadouble <yadouble@student.42.fr>
2019-09-02 14:47:55 +0200

    ImdbID YTSid

--- utils/moviesUtils.js
+++ utils/moviesUtils.js
@@ -5,8 +5,8 @@ const {RAPIDAPI_KEY} = require('../config/apiKey');
 // const limitedRequest = rateLimit(axios.create(), {maxRequests: 35, perMilliseconds: 10000});
 
 module.exports = {
-    getMovieInfo: async (id) => {
-        return await axios.get(`https://movie-database-imdb-alternative.p.rapidapi.com/?i=${id}&r=json`, {
+    getMovieInfo: async (IMDBid, YTSid) => {
+        return await axios.get(`https://movie-database-imdb-alternative.p.rapidapi.com/?i=${IMDBid}&r=json`, {
             headers: {
                 "X-RapidAPI-Host": "movie-database-imdb-alternative.p.rapidapi.com",
                 "X-RapidAPI-Key": RAPIDAPI_KEY
@@ -15,7 +15,9 @@ module.exports = {
             let genres = await translateSentence(res.data.Genre);
             res.data.Genre = genres.split(',').map((genre) => (genre.trim()));
             res.data.Plot = await translateSentence(res.data.Plot);
-            return res.data;
+            let yts = await axios.get(`https://yts.lt/api/v2/movie_details.json?movie_id=${YTSid}`);
+            res.data.yts = yts ? yts.data.data.movie : {};
+            return res.data.imdbID === res.data.yts.imdb_code ? res.data : null;
         })
     },
     filterByGenre: (movies, category) => {
