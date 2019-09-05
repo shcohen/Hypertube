d0339ad20d6cb35216bff681344803d2d04ae27f
Shana COHEN <shcohen@e1r5p15.42.fr>
2019-09-05 11:35:48 +0200

    check pp en cours

--- server/apiRouter.js
+++ server/apiRouter.js
@@ -28,6 +28,9 @@ exports.router = (() => {
     apiRouter.route('/search/:id/submit').get(postManagement.createComment);
     apiRouter.route('/search/:id/modify').get(postManagement.modifyComment);
     apiRouter.route('/search/:id/delete').get(postManagement.deleteComment);
+    apiRouter.route('/search/:id/favorites').get(postManagement.addMovieToFav);
+    apiRouter.route('/favorites').get(postManagement.displayFavMovies);
+
 
     return apiRouter;
 })();
--- server/routes/postManagement.js
+++ server/routes/postManagement.js
@@ -1,8 +1,68 @@
 const Comment = require('../models/comment');
 const User = require('../models/user');
+const favMovie = require('../models/favmovie');
 const xss = require('xss');
 
 module.exports = {
+    addMovieToFav: (req, res) => {
+        let {acc_id, movie_id} = req.body;
+        if (!acc_id || !movie_id) {
+            return res.status(400).send('error: invalid request')
+        } else {
+            User.findOne({
+                acc_id: acc_id
+            }).then((user, error) => {
+                if (error) {
+                    console.log(error);
+                    return res.status(401).send('error: account not found')
+                } else if (user) {
+                    favMovie.create({
+                        acc_id: acc_id,
+                        movie_id: movie_id
+                    }).then((isAdded, error) => {
+                        if (error) {
+                            console.log(error);
+                            return res.status(500).send('error: movie was not added to favorites')
+                        } else if (isAdded) {
+                            return res.status(201).send('success: movie was added to Favorites')
+                        }
+                    })
+                }
+            })
+        }
+    },
+    displayFavMovies: (req, res) => {
+        let {acc_id} = req.body;
+        if (!acc_id) {
+            return res.status(400).send('error: invalid request')
+        } else {
+            User.findOne({
+                acc_id: acc_id
+            }).then((user, error) => {
+                if (error) {
+                    console.log(error);
+                    return res.status(401).send('error: account not found')
+                } else if (user) {
+                    favMovie.find({
+                        acc_id: acc_id
+                    }).then((favMovies, error) => {
+                        if (error) {
+                            console.log(error);
+                            return res.status(400).send('error: invalid request')
+                        } else if (favMovies) {
+                            let favMaps = {};
+                            favMovies.forEach((fav) => {
+                                favMaps[fav._id] = fav;
+                            });
+                            return res.send(favMaps)
+                        }
+                    })
+                } else {
+                    return res.status(200).send('no movies was added as favorites');
+                }
+            })
+        }
+    },
     createComment: (req, res) => {
         let {acc_id, movie_id, message} = req.body;
         if (!acc_id || !movie_id || !message) {
@@ -32,61 +92,63 @@ module.exports = {
             })
         }
     },
-    modifyComment: (req, res) => {
-        let {post_id, acc_id, movie_id, message} = req.body;
-        if (!post_id || !acc_id || !movie_id || !message) {
-            return res.status(400).send('error: invalid request')
-        } else {
-            User.findOne({
-                acc_id: acc_id
-            }).then((user, error) => {
-                if (error) {
-                    console.log(error);
-                    return res.status(401).send('error: account not found')
-                } else if (user) {
-                    Comment.findOneAndUpdate({
-                        post_id: xss(post_id),
-                        acc_id: xss(acc_id),
-                        movie_id: xss(movie_id),
-                        message: xss(message)
-                    }).then((isModified, error) => {
-                        if (error) {
-                            console.log(error);
-                            return res.status(500).send('error: comment not modified')
-                        } else if (isModified) {
-                            return res.status(200).send('success: comment modified')
-                        }
-                    })
-                }
-            })
+    modifyComment:
+        (req, res) => {
+            let {post_id, acc_id, movie_id, message} = req.body;
+            if (!post_id || !acc_id || !movie_id || !message) {
+                return res.status(400).send('error: invalid request')
+            } else {
+                User.findOne({
+                    acc_id: acc_id
+                }).then((user, error) => {
+                    if (error) {
+                        console.log(error);
+                        return res.status(401).send('error: account not found')
+                    } else if (user) {
+                        Comment.findOneAndUpdate({
+                            post_id: xss(post_id),
+                            acc_id: xss(acc_id),
+                            movie_id: xss(movie_id),
+                            message: xss(message)
+                        }).then((isModified, error) => {
+                            if (error) {
+                                console.log(error);
+                                return res.status(500).send('error: comment not modified')
+                            } else if (isModified) {
+                                return res.status(200).send('success: comment modified')
+                            }
+                        })
+                    }
+                })
+            }
+        },
+    deleteComment:
+        (req, res) => {
+            let {post_id, acc_id} = req.body;
+            if (!post_id || !acc_id) {
+                console.log(req.body);
+                return res.status(400).send('error: invalid request')
+            } else {
+                User.findOne({
+                    acc_id: acc_id
+                }).then((user, error) => {
+                    if (error) {
+                        console.log(error);
+                        return res.status(401).send('error: account not found')
+                    } else if (user) {
+                        Comment.findOneAndDelete({
+                            post_id: xss(post_id),
+                            acc_id: xss(acc_id)
+                        }).then((isDeleted, error) => {
+                            if (error) {
+                                console.log(error);
+                                return res.status(500).send('error: comment not deleted')
+                            } else if (isDeleted) {
+                                return res.status(200).send('success: comment deleted')
+                            }
+                        })
+                    }
+                })
+            }
         }
-    },
-    deleteComment: (req, res) => {
-        let {post_id, acc_id} = req.body;
-        if (!post_id || !acc_id) {
-            console.log(req.body);
-            return res.status(400).send('error: invalid request')
-        } else {
-            User.findOne({
-                acc_id: acc_id
-            }).then((user, error) => {
-                if (error) {
-                    console.log(error);
-                    return res.status(401).send('error: account not found')
-                } else if (user) {
-                    Comment.findOneAndDelete({
-                        post_id: xss(post_id),
-                        acc_id: xss(acc_id)
-                    }).then((isDeleted, error) => {
-                        if (error) {
-                            console.log(error);
-                            return res.status(500).send('error: comment not deleted')
-                        } else if (isDeleted) {
-                            return res.status(200).send('success: comment deleted')
-                        }
-                    })
-                }
-            })
-        }
-    }
 };
