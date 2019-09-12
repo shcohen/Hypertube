c83342696ed745b97b70fcc17fcbc9d585f1eff1
yadouble <yadouble@student.42.fr>
2019-09-12 19:34:23 +0200

    Le stage est sain et sauf

--- server/utils/moviesUtils.js
+++ server/utils/moviesUtils.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const fs = require('fs');
 const rimraf = require('rimraf');
+const popcornTimeModel = require('../models/popcornTime');
 const downloadedMovies = require('../models/downloadedMovies');
 const watchedMovie = require('../models/watchedMovie');
 const {translateSentence} = require('./languageUtils');
@@ -17,10 +18,28 @@ module.exports = {
             // let genres = await translateSentence(res.data.Genre);
             // res.data.Genre = genres.split(',').map((genre) => (genre.trim()));
             // res.data.Plot = await translateSentence(res.data.Plot);
-            let yts = await axios.get(`https://yts.lt/api/v2/movie_details.json?movie_id=${YTSid}`);
-            res.data.yts = yts ? yts.data.data.movie : {};
-            // res.data.yts.description_full =  await translateSentence(res.data.yts.description_full);
-            return res.data.imdbID === res.data.yts.imdb_code ? res.data : null;
+            if (YTSid !== 'undefined') {
+                let yts = await axios.get(`https://yts.lt/api/v2/movie_details.json?movie_id=${YTSid}`);
+                // res.data.yts.description_full =  await translateSentence(res.data.yts.description_full);
+                res.data.yts = yts ? yts.data.data.movie : {};
+            }
+            let popcornTime = await popcornTimeModel.find({imdb_code: IMDBid});
+            res.data.popcornTime = popcornTime ? popcornTime[0] : {};
+            return res.data.imdbID === res.data.popcornTime.imdb_code ? res.data : null;
+        })
+    },
+    filterDuplicateMovies: async (ytsMovies, popcornMovies) => {
+        let mixedMovies = [...ytsMovies, ...popcornMovies];
+        return mixedMovies.filter((thing, i, self) => self.findIndex(t => t.imdb_code === thing.imdb_code) === i);
+    },
+    watchedMovieInfo: async (IMDBid) => {
+        return await axios.get(`https://movie-database-imdb-alternative.p.rapidapi.com/?i=${IMDBid}&r=json`, {
+            headers: {
+                "X-RapidAPI-Host": "movie-database-imdb-alternative.p.rapidapi.com",
+                "X-RapidAPI-Key": RAPIDAPI_KEY
+            }
+        }).then(async res => {
+            return res.data;
         })
     },
     trackWatchedMovie: async (accId, movieId) => {
