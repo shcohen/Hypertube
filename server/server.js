83aad13c1bcf401a634333737daf98d71e555874
Florent Klein <flklein@student.42.fr>
2019-09-14 18:43:55 +0200

    deprecate mongoose

--- server/server.js
+++ server/server.js
@@ -17,7 +17,11 @@ new CronJob('0 0 * * * *', () => {
 }, null, true, 'Europe/Paris');
 
 // connecting to database
-mongoose.connect('mongodb+srv://root:root@hypertube-yfmfl.mongodb.net/Hypertube?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect('mongodb+srv://root:root@hypertube-yfmfl.mongodb.net/Hypertube?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+})
   .then(() => {
       console.log('Connected to mongoDB')
   }).catch(e => {
